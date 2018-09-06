83ab7fcc1440fa16216b78420748f29e3ac2d8d1
Rogelio Orona <rogelio.orona@thomsonreuters.com>
2018-09-06 12:41:21 -0500

    Chapter 1 - Arrays and Strings

---
+++ src/main/java/com/roger/chapter1/IsUnique.java
@@ -0,0 +1,12 @@
+package com.roger.chapter1;
+
+/**
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures?
+ *
+ * Hints: #44, #117, #132
+ */
+
+public class IsUnique {
+
+}
